2b05afb57b7eac7212077f7fa9357a8666386299
David Gardner <david_lng8@yahoo.com>
2023-11-12 22:36:48 -0500

    Added font awesome and setup navbar

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,7 +1,16 @@
+import React from 'react'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faCirclePlus } from '@fortawesome/free-solid-svg-icons'
+
 export default function NavBar() {
-    return (
-      <nav className="bg-red-200 pb-2 pt-2 text-3xl font-bold">
-        Hello world!
+  return (
+    <>
+      <nav className="bg-gray-800 text-white p-4 flex justify-between items-center">
+        <div className="text-3xl font-bold flex-1 text-center">Squad TechStore</div>
+        <button className="bg-green-700 text-white px-4 py-2 rounded hover:bg-blue-600">
+          Add Item <FontAwesomeIcon icon={faCirclePlus} beatFade />
+        </button>
       </nav>
-    )
-  }
+    </>
+  )
+}
